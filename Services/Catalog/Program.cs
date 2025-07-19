076c01c5e7daa9c4c86778fb7901b11fe2d5a902
vibhashan <ranhindavibhashana@gmail.com>
2025-07-19 16:44:04 +0530

    feat(catalog): initialization
    Initialized catalog service folder structure

---
+++ Services/Catalog/Program.cs
@@ -0,0 +1,23 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Catalog.API", Version = "2.0" }); });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapGet("/", () =>
+{
+    return "Hello World ssss";
+});
+
+app.Run();
+
